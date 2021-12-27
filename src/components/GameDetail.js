ab2acc45ee0e66052ad3426245393c5d249c6538
Vivek <57166545viviek00134@users.noreply.github.com>
2021-12-27 16:22:12 +0530

    Added Platform logos

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -6,6 +6,13 @@ import { motion } from "framer-motion";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { smallImage } from "../util";
+//IMAGES
+import playstation from "../img/playstation.svg";
+import steam from "../img/steam.svg";
+import xbox from "../img/xbox.svg";
+import nintendo from "../img/nintendo.svg";
+import apple from "../img/apple.svg";
+import gamepad from "../img/gamepad.svg";
 
 const GameDetail = ({ pathId }) => {
   const navigate = useNavigate();
@@ -17,6 +24,22 @@ const GameDetail = ({ pathId }) => {
       navigate("/");
     }
   };
+  //Get platform images
+  const getPlatform = (platform) => {
+    switch (platform) {
+      case "PlayStation 4":
+        return playstation;
+      case "Xbox One":
+        return xbox;
+      case "Nintendo switch":
+        return nintendo;
+      case "iOS":
+        return apple;
+      default:
+        return gamepad;
+    }
+  };
+
   //Data
   const { screen, game, isLoading } = useSelector((state) => state.detail);
 
@@ -34,7 +57,11 @@ const GameDetail = ({ pathId }) => {
                 <h3>Platforms</h3>
                 <Platforms>
                   {game.platforms.map((data) => (
-                    <h3 key={data.platform.id}>{data.platform.name}</h3>
+                    <img
+                      key={data.platform.id}
+                      src={getPlatform(data.platform.name)}
+                      alt="Platform"
+                    ></img>
                   ))}
                 </Platforms>
               </Info>
